6662fb03d55488dcc764f59bd11fdf994b3434d4
Alois Heloir <alois.heloir@live.fr>
2021-10-30 11:38:50 +0200

    Add precommit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-const helloWord = (): void => {
-  console.log('Hello world !');
-};
+import {helloWorld} from './hello-world';
 
-helloWord();
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req: any, res: any) => {
+  res.send(helloWorld());
+});
+
+app.listen(port, () => {
+  console.log(helloWorld());
+});
