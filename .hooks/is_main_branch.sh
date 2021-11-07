ce5199b535648906eb5e40fa7c612927af0d043d
Loup Dallier <loup.dallier@epita.fr>
2021-11-07 20:16:44 +0100

    add yarn command to install dependencies

--- .hooks/is_main_branch.sh
+++ .hooks/is_main_branch.sh
@@ -5,5 +5,5 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 if [ "$branch" == "main" ]
 then
 	    echo "Not allowed to commit/push directly on main. Please use pull request"
-	        exit 1
+	    exit 1
 fi
