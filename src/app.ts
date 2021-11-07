4b8b9a1275988bd241f6e742d64457b05a02d81d
Alois Heloir <alois.heloir@live.fr>
2021-11-07 19:18:36 +0100

    Clean code

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,3 @@
 const express = require('express');
 export const app = express();
+app.use(express.json());
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { sequelize } from 'src/app-sequelize';
-import { helloWorld } from 'src/hello-world';
 import { app } from 'src/app';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { UserController } from 'src/api/user/user.controller';
-
-new UserController();
 
 const port = 8080;
+
 app.listen(port, () => {
-  console.log(helloWorld());
+  console.log('Hello World!');
   sequelize
     .authenticate()
     .then(() => {
